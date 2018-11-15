db8f53959a87516dc0e97d1c853be2e8af9f8fb8
Joah Gerstenberg <me@joahg.com>
2018-11-15 15:33:03 -0600

    use formadata and json bodyparsers

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.text());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Everything is 200 OK');
